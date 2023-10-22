903171503e6fc240c148aaf1f9f3e7b5556ea60d
divyanshupundir <divyanshu.pundir@gmail.com>
2023-10-22 20:26:27 +0530

    feat: Add coroutines adapter and remove default scheduler from rx adapters

--- adapter-rxjava2/src/main/kotlin/com/divpundir/websockt/adapter/rxjava2/create.kt
+++ adapter-rxjava2/src/main/kotlin/com/divpundir/websockt/adapter/rxjava2/create.kt
@@ -2,13 +2,9 @@ package com.divpundir.websockt.adapter.rxjava2
 
 import com.divpundir.websockt.WebSocketFactory
 import com.divpundir.websockt.WebSockt
-import io.reactivex.Scheduler
-import io.reactivex.schedulers.Schedulers
 
 public fun WebSockt.createRx2Client(
     factory: WebSocketFactory,
-    scheduler: Scheduler = Schedulers.io()
 ): Rx2WebSocketClient = Rx2WebSocketClientImpl(
-    factory,
-    scheduler
+    factory
 )
--- adapter-rxjava3/src/main/kotlin/com/divpundir/websockt/adapter/rxjava3/create.kt
+++ adapter-rxjava3/src/main/kotlin/com/divpundir/websockt/adapter/rxjava3/create.kt
@@ -2,13 +2,9 @@ package com.divpundir.websockt.adapter.rxjava3
 
 import com.divpundir.websockt.WebSocketFactory
 import com.divpundir.websockt.WebSockt
-import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 public fun WebSockt.createRx3Client(
     factory: WebSocketFactory,
-    scheduler: Scheduler = Schedulers.io()
 ): Rx3WebSocketClient = Rx3WebSocketClientImpl(
     factory,
-    scheduler
 )
