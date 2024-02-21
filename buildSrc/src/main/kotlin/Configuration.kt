e4017857d1b58799cd20288da13336ae1fa527f4
divyanshupundir <divyanshu.pundir@gmail.com>
2024-02-21 22:59:11 +0530

    build: Update dependencies and version bump

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,6 +2,6 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val group = "com.divpundir.websockt"
-    const val version = "0.4.0"
+    const val version = "0.5.0"
     val javaVersion = JavaVersion.VERSION_1_8
 }
